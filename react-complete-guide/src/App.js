46c6374a041d42b31ffe3dcc819ef8a52a7e31ae
Bishnu Das <bishnu.das@outlook.in>
2018-07-05 22:37:33 +0530

    Using pseudo selectors with radium

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -88,7 +88,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     //console.log('render() is called');
@@ -127,6 +131,10 @@ class App extends Component {
       )
 
       style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
