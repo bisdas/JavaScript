caa12c993e3e4853ac05f6aa327d750a77d16ae2
Bishnu Das <bishnu.das@outlook.in>
2018-07-13 14:41:08 +0530

    Webpack tweaking for scoped CSS

--- react-complete-guide/config/webpack.config.prod.js
+++ react-complete-guide/config/webpack.config.prod.js
@@ -181,6 +181,8 @@ module.exports = {
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
+                        modules: true,
+                        localIdentName: '[name__[local]__[hash:base64:5]',
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
                       },
