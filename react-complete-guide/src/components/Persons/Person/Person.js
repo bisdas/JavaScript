0193821db77502783f7337d6b75b1d56cb791333
Bishnu Das <bishnu.das@outlook.in>
2018-07-15 22:30:57 +0530

    Convert Person to class based component

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css'
 
-const person = (props) => {
-    //const rnd = Math.random();
-    // if(rnd > 0.9){
-    //     throw new Error('Something went wrong');
-    // }
-    return (
-        <div className={classes.Person}>
-            <p onClick={ props.click }>I'm { props.name } and I'm { props.age } years old!</p>
-            <p>{ props.children }</p>
-            <input type="text" onChange={props.change} value={props.name} />
-        </div>
-    )
+class Person extends Component {
+    render() {
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.change} value={this.props.name} />
+            </div>
+        );
+    }
 }
 
-export default person;
+export default Person;
