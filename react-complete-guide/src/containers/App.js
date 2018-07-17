29b9961538599012a31b257362a5e3004d1c2417
Bishnu Das <bishnu.das@outlook.in>
2018-07-17 23:43:54 +0530

    A better way to use setState()

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -17,7 +17,8 @@ class App extends PureComponent {
                 { id: 'sfdsfd', name: 'Jaya', age: 18 }
             ],
             otherState: 'some other value',
-            showPersons: false
+            showPersons: false,
+            toggleClicked: 0
         }
     }
 
@@ -73,7 +74,12 @@ class App extends PureComponent {
 
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
-        this.setState({ showPersons: !doesShow });
+        this.setState((prevState, props) => {
+            return {
+                showPersons: !doesShow,
+                toggleClicked: prevState.toggleClicked + 1
+            }
+        });
     }
 
     deletePersonsHandler = (personIndex) => {
