46b7a24df5568753502726902e9a8c43f22c1c0b
Bishnu Das <bishnu.das@outlook.in>
2018-07-17 14:20:10 +0530

    Returning adjacent JSX elements as array.

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -18,13 +18,19 @@ class Person extends Component {
     render() {
         console.log('[Person.js] Inside render()');
 
-        return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.change} value={this.props.name} />
-            </div>
-        );
+        // return (
+        //     <div className={classes.Person}>
+        //         <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
+        //         <p>{this.props.children}</p>
+        //         <input type="text" onChange={this.props.change} value={this.props.name} />
+        //     </div>
+        // );
+
+        return [
+            <p key="1" onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>,
+            <p key="2">{this.props.children}</p>,
+            <input key="3" type="text" onChange={this.props.change} value={this.props.name} />
+        ]
     }
 }
 
