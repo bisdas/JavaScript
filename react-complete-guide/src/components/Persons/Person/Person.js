28f72518bfc1f1c002db52006d321733decdf188
Bishnu Das <bishnu.das@outlook.in>
2018-07-19 22:35:39 +0530

    Using ref to focus an input element

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -17,6 +17,7 @@ class Person extends Component {
 
     componentDidMount() {
         console.log('[Person.js] Inside componentDidMount()');
+        this.inputElement.focus();
     }
 
     render() {
@@ -26,7 +27,11 @@ class Person extends Component {
             <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.change} value={this.props.name} />
+                <input
+                    ref={(inp) => { this.inputElement = inp }}
+                    type="text"
+                    onChange={this.props.change}
+                    value={this.props.name} />
             </Aux>
         );
 
