fea7115b32d226f8587879b2be166e4b3dbd4cc2
Bishnu Das <bishnu.das@outlook.in>
2018-07-30 21:59:19 +0530

    [React] Rectify the error where the ORDER button does not get enabled on first ingredient add.

--- React/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ React/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,10 +22,10 @@ class BurgerBuilder extends Component {
         purchaseable: false
     }
 
-    updatePurchaseState() {
-        const ingredients = {
-            ...this.state.ingredients
-        }
+    updatePurchaseState(ingredients) {
+        // const ingredients = {
+        //     ...this.state.ingredients
+        // }
 
         const sum = Object.keys(ingredients)
             .map(igKey => {
@@ -50,7 +50,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-        this.updatePurchaseState();
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -70,7 +70,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-        this.updatePurchaseState();
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render() {
