d8427432074044704f9154db36ef27049ee3fecd
Das, Bishnum <bishnu.das@outlook.in>
2018-08-11 12:31:11 +0530

    [React] Added routes to 'BurgerBuilder' and 'Checkout' components

--- React/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ React/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,26 +93,28 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     }
 
-    purchaseCContinueHandler = () => {
-        this.setState({ loading: true });
+    purchaseContinueHandler = () => {
+        // this.setState({ loading: true });
 
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Bishnu',
-                address: {
-                    street: 'Test Address 1',
-                    zipCode: '234324',
-                    country: 'India'
-                },
-                email: 'test@test.com',
-                deliveryMethod: 'fastest'
-            }
-        }
-        axios.post('/orders.json', order)
-            .then(response => this.setState({ loading: false, purchasing: false }))
-            .catch(error => this.setState({ loading: false, purchasing: false }));
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Bishnu',
+        //         address: {
+        //             street: 'Test Address 1',
+        //             zipCode: '234324',
+        //             country: 'India'
+        //         },
+        //         email: 'test@test.com',
+        //         deliveryMethod: 'fastest'
+        //     }
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => this.setState({ loading: false, purchasing: false }))
+        //     .catch(error => this.setState({ loading: false, purchasing: false }));
+
+        this.props.history.push('/checkout');
     }
 
     render() {
@@ -145,7 +147,7 @@ class BurgerBuilder extends Component {
                 ingredients={this.state.ingredients}
                 price={this.state.totalPrice}
                 purchaseCancelled={this.purchaseCancelHandler}
-                purchaseContinued={this.purchaseCContinueHandler} />
+                purchaseContinued={this.purchaseContinueHandler} />
         }
 
         if (this.state.loading) {
