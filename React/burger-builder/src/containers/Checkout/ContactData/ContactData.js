e031b170a6ec014b4aa28dc65685318a05854866
Das, Bishnum <bishnu.das@outlook.in>
2018-08-18 00:52:26 +0530

    [React] creating the form elements dynamically from config

--- React/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ React/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -91,12 +91,24 @@ class ContactData extends Component {
     }
 
     render() {
+        let formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-                <Input elementType="..." elementConfig="" value="..." />
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} />
+                ))}
+
                 <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
             </form>
         );
