b0ed7caca885cde4af52ff0fb7a4967dc048809d
Das, Bishnum <bishnu.das@outlook.in>
2018-08-18 01:38:59 +0530

    [React] Setting up two-way binding with the onChange handler.

--- React/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ React/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -90,6 +90,21 @@ class ContactData extends Component {
 
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        this.setState({ orderForm: updatedOrderForm });
+    }
+
     render() {
         let formElementsArray = [];
         for (let key in this.state.orderForm) {
@@ -106,7 +121,8 @@ class ContactData extends Component {
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value} />
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
 
                 <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
