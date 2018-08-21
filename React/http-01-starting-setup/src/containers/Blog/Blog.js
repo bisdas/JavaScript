4cb588dd59d54efa1f170d349eb15fbcb2305219
Das, Bishnum <bishnu.das@outlook.in>
2018-08-21 23:05:32 +0530

    [React] using 'Switch' component to prevent multiple route parsing

--- React/http-01-starting-setup/src/containers/Blog/Blog.js
+++ React/http-01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -52,10 +52,10 @@ class Blog extends Component {
                 */}
 
                 <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" component={FullPost} />
-
-                {/* <Posts /> */}
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" component={FullPost} />
+                </Switch>
             </div>
         );
     }
