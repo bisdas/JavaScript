db0eec545b567c2d21d1d8f88443da900ef91fca
Das, Bishnum <bishnu.das@outlook.in>
2018-08-21 22:33:50 +0530

    [React] reading params from props.

--- React/http-01-starting-setup/src/components/Post/Post.js
+++ React/http-01-starting-setup/src/components/Post/Post.js
@@ -6,7 +6,7 @@ const post = (props) => {
     console.log(props);
     return (
         <article className="Post" onClick={props.clicked}>
-            <h1>{props.title}}</h1>
+            <h1>{props.title}</h1>
             <div className="Info">
                 <div className="Author">{props.author}</div>
             </div>
