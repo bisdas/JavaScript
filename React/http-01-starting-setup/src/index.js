ad02f1cb13dbb5c11cccbf8ae57f83dbc66e3a44
Das, Bishnum <bishnu.das@outlook.in>
2018-08-23 15:30:56 +0530

    [React] Revert commit #4428258

--- React/http-01-starting-setup/src/index.js
+++ React/http-01-starting-setup/src/index.js
@@ -1,40 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
 // axios global configuration
-axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
-axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
-axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-//axios interceptors
+
+//axios interceptors 
 axios.interceptors.request.use(
-  request => {
-    console.log(request);
+    request => {
+        console.log(request);
 
-    //edit request config
-    return request;
-  },
-  error => {
-    console.log(error);
-    return Promise.reject(error);
-  }
+        //edit request config
+        return request;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
 );
 
 axios.interceptors.response.use(
-  response => {
-    console.log(response);
-    return response;
-  },
-  error => {
-    console.log(error);
-    return Promise.reject(error);
-  }
+    response => {
+        console.log(response);
+        return response;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
 );
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
