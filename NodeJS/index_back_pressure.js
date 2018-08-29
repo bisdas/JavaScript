05460828146d43e21084b94b8b2a684dc9c1832a
Bishnu Das <bishnu.das@outlook.in>
2018-08-29 14:39:04 +0530

    [Node] back pressure example

---
+++ NodeJS/index_back_pressure.js
@@ -0,0 +1,21 @@
+//Back presure
+
+const fs = require('fs');
+
+const readerStream = fs.createReadStream('./big.file', 'utf8');
+const writeStream = fs.createWriteStream('./bigfileCopy.txt');
+let data=''
+//read operation
+readerStream.on('data', function (chunk) {
+    const buffer_good = writeStream.write(chunk);
+    if (!buffer_good) readerStream.pause();
+
+})
+writeStream.on('drain', function () {
+    console.log('buffer drained!');
+    readerStream.resume();
+});
+
+readerStream.on('end', function () {
+    console.log('end is called');
+});
