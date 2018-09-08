3226d06bfeca29384ffa8aeef25cfe97b8953338
Das, Bishnum <bishnu.das@outlook.in>
2018-09-08 19:09:57 +0530

    [React] adding App component

--- React/react-todo-list/src/index.tsx
+++ React/react-todo-list/src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 const ROOT = document.querySelector('.container');
-ReactDOM.render(<h1>Hello two</h1>, ROOT);
+ReactDOM.render(<App name="Kofi" />, ROOT);
