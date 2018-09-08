3f9a90a68fa96b74a31705bb074dcc18695a0e4a
Das, Bishnum <bishnu.das@outlook.in>
2018-09-08 20:36:46 +0530

    [React] configure scss loader and use css

--- React/react-todo-list/src/components/App.tsx
+++ React/react-todo-list/src/components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-class App extends React.Component<IProps, {}>{
+export class App extends React.Component<IProps, {}>{
     render() {
         return <h1>Hello {this.props.name}</h1>
     }
@@ -9,5 +9,3 @@ class App extends React.Component<IProps, {}>{
 interface IProps {
     name: string
 }
-
-export default App;
--- React/react-todo-list/src/index.tsx
+++ React/react-todo-list/src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import App from './components/App';
+import { App } from './components/App';
+import './styles/style.scss';
 
 const ROOT = document.querySelector('.container');
 ReactDOM.render(<App name="Kofi" />, ROOT);
