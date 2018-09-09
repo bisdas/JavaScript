d0bcdf1404065dd316c707e8acfd5648fc6fce48
Das, Bishnum <bishnu.das@outlook.in>
2018-09-09 21:23:30 +0530

    [React] add styles to the elements

--- React/react-todo-list/src/components/App.tsx
+++ React/react-todo-list/src/components/App.tsx
@@ -47,23 +47,24 @@ export class App extends React.Component<{}, IState.default>{
     public renderTasks = (): JSX.Element[] => {
         return this.state.tasks.map((task: ITask, index: number) => {
             return (
-                <div key={task.id}>
-                    <span>{task.value}</span>
+                <div key={task.id} className="tdl-task">
+                    <span className={task.completed ? 'is-completed' : ''}>{task.value}</span>
                     <button onClick={() => this.deleteTask(task.id)}>Delete</button>
-                    <button onClick={() => this.toggleDone(index)}>Done</button>
+                    <button onClick={() => this.toggleDone(index)}>{task.completed ? 'Undo' : 'Done'}</button>
                 </div>
             )
         });
     }
 
     public render(): JSX.Element {
-        console.log(this.state);
+        console.log(this.state.tasks);
         return (
             <div>
                 <h1>React TypeScript Todo List</h1>
                 <form onSubmit={(e) => this.handleSubmit(e)}>
                     <input
                         type="text"
+                        className="tdl-input"
                         placeholder="Add a task"
                         value={this.state.currentTask}
                         onChange={(e) => this.setState({ currentTask: e.target.value })}>
