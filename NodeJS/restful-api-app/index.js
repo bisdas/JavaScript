b48e840351cc12fe25ee0241f8958d6756257851
Das, Bishnum <bishnu.das@outlook.in>
2019-03-03 13:19:04 +0530

    [Node] parsing the request path

--- NodeJS/restful-api-app/index.js
+++ NodeJS/restful-api-app/index.js
@@ -4,13 +4,35 @@
 
 
 // Dependencies
-const http = require('http');
+const http = require('http'); // built in node module related to http operations
+const url = require('url'); // built in node module related to url 
 
-
+// the server should respond to all requests with a string
 const server = http.createServer(function (request, response) {
-    response.end('Hello World...!');
-})
+    //get the url and parse it
+    //console.log((new Date).toString(), ': Request URL', request.url);
+    const parsedUrl = url.parse(request.url, true);
 
+    
+    //get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    
+    // send the response
+    response.end('Hello World...!');
+    
+
+    //log the request path
+    console.log((new Date).toString(), ': Request received with path: ', trimmedPath);
+});
+
+// the server should log whena request arrives
+server.on('request', function (request, response) {
+    console.log((new Date).toString(), ': Request received with URL', request.url);
+});
+
+// the server should listen to port 3000
 server.listen(3000, function () {
-    console.log('Server is now listening on port 3000');
-})
+    console.log((new Date).toString(), ': Server listening on port 3000');
+});
