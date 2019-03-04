b74bbf9b75795b54b9fce59314da6320b2237041
Das, Bishnum <bishnu.das@outlook.in>
2019-03-04 08:06:04 +0530

    [Node] reading the payload

--- NodeJS/restful-api-app/index.js
+++ NodeJS/restful-api-app/index.js
@@ -6,16 +6,18 @@
 // Dependencies
 const http = require('http'); // built in node module related to http operations
 const url = require('url'); // built in node module related to url 
+const StringDecoder = require('string_decoder').StringDecoder; // built in node module to decode strings
 
 // the server should respond to all requests with a string
 const server = http.createServer(function (request, response) {
     console.clear();
-    
+    console.log("Request Time:", (new Date).toString());
+
     //get the url and parse it
     //console.log('Request URL', request.url);
     const parsedUrl = url.parse(request.url, true);
 
-    
+
     //get the path
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
@@ -23,18 +25,33 @@ const server = http.createServer(function (request, response) {
 
     // get the request method
     const method = request.method.toLowerCase();
-    
+
 
     //get the querystring parameters
     const queryStringObject = parsedUrl.query;
-    
+
 
     //get the request headers
     const headers = request.headers;
-    
-    // send the response
-    response.end('Hello World...!');
-    
+
+
+    //reading the payload
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+    request.on('data', function (data) {
+        console.log('Data received:', data);
+        buffer += decoder.write(data);
+    });
+
+    request.on('end', function () {
+        console.log('Data end:', decoder.end());
+        buffer += decoder.end();
+        // send the response
+        response.end('Hello World...!');
+        console.log('Request payload: ', buffer);
+    });
+
 
     //log the request data
     console.log('Request path: ', trimmedPath);
