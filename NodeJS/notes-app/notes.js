fe25b7806030879df9dcc86f57df84ddc02415fe
Bishnu Das <16491515+bisdas@users.noreply.github.com>
2019-03-14 00:06:18 +0530

    [Node] reading a note

--- NodeJS/notes-app/notes.js
+++ NodeJS/notes-app/notes.js
@@ -25,23 +25,6 @@ const addNote = (title, body) => {
     }
 }
 
-const saveNotes = (notes) => {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
-}
-
-const loadNotes = () => {
-    try{
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
-    }
-    catch (e){
-        return []
-    }
-}
-
-
 const removeNote = (title) => {
     const notes = loadNotes()
     const notesToKeep = notes.filter((note) => note.title !== title)
@@ -69,10 +52,41 @@ const listNotes = () =>{
     // }
 }
 
+const readNote = (title) => {
+    const notes = loadNotes();
+    const note = notes.find((note) => note.title === title)
+
+    if(note){
+        console.log(chalk.blue.inverse(note.title))
+        console.log(note.body)
+    }
+    else{
+        console.log(chalk.red.inverse('Note not found'))
+    }
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    }
+    catch (e){
+        return []
+    }
+}
+
+
 
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 };
